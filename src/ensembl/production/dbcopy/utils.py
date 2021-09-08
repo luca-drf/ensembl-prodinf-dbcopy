dfb18b914a269dd3beccf88a49c43892c4f07216
Luca Da Rin Fioretto <ldrf@ebi.ac.uk>
2021-09-08 18:07:24 +0100

    utils.get_filters returns set instead of list

--- src/ensembl/production/dbcopy/utils.py
+++ src/ensembl/production/dbcopy/utils.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 
 def get_filters(values):
-    named_filters = [v.replace('%', '.*') for v in values if v]
-    logger.debug("from [%s]", values)
+    named_filters = {v.replace('%', '.*') for v in values if v}.difference({''})
+    logger.debug("from %s", values)
     logger.debug("filters %s", named_filters)
     return named_filters
